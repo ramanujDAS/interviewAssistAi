92fc07878d7bded4ce8a0411d263aa8e232b2988
ramanujsingh <ramanuj3rd@gmail.com>
2026-05-27 23:37:42 +0530

    latest

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          userName: loginUsername,
+          username: loginUsername,
           password: loginPassword,
         }),
       });
